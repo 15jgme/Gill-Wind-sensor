7c259d0bc0129709e29faf9e2d72538ad012a214
Jackson Empey <47725944+15jgme@users.noreply.github.com>
2021-05-12 11:25:47 -0400

    added time led switch

--- windsonic.py
+++ windsonic.py
@@ -13,13 +13,12 @@ dataIter = 1
 dataName_base = "test_"
 dataName = "test_1"
 
-# def utf8len(s): #For counting bytes for checksum
-#     return len(s.encode('utf-8'))
-
-#Want to only log data when switch on gpio
 
 switch = Button(4) #Init switch for data logging
 
+ledInd = 0 #LED interator for use with blinking
+
+#FILE CREATION AND HEADERS
 log = True
 if log:
     while path.exists(dataName + ".txt"): #Check to see if our new file name is unique
@@ -30,20 +29,36 @@ if log:
     f.write("DATA FORMAT")
     f.write("Format, Wind Direction, Wind Speed, Units, Status, Checksum")
 
+#ACTIVATE LED
+os.system("echo none > /sys/class/leds/led0/trigger")
 
-try:
-    while True:
-        newDat = ser.readline()
-        newDat2 = newDat.replace(b"<STX>", b"")
-        newDat3 = newDat.replace(b"<ETX>", b"")
-        # byteChk = utf8len(newDat3)
-        f.write(str(time.process_time()) + str(newDat3))
-        sys.stdout.write(str(newDat3)+"\r")
-        sys.stdout.flush()
+def ledOn():
+    os.system("echo 1 >/sys/class/leds/led0/brightness")
+def ledOff():
+    os.system("echo 0 >/sys/class/leds/led0/brightness")
 
-except KeyboardInterrupt:
-    print("Press Ctrl-C to terminate while statement")
-    pass
+#DATA LOGGING SECTION
+while True: #Logging loop
+    try:
+        if switch.is_active: #Logging switch is flicked 
+            #Only log when switch is active
+            newDat = ser.readline()
+            newDat2 = newDat.replace(b"<STX>", b"")
+            newDat3 = newDat.replace(b"<ETX>", b"")
+            # byteChk = utf8len(newDat3)
+            toWrite = str(time.process_time()) + "," + str(newDat3)
+            f.write(toWrite)
+            sys.stdout.write(toWrite+"\r")
+            sys.stdout.flush()
+            
+            if ledInd % 40 == 0:
+                ledOn()
 
+            if ledInd % 19 == 0:
+                ledOff()
+    
+    except KeyboardInterrupt:
+        print("Pressed Ctrl-C to terminate while statement")
+        break
 
 
