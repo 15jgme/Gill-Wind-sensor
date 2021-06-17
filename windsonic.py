ae21720886835a1fa5fe5dbf3370c67fd53d29b5
Jackson <15jgme@queensu.ca>
2021-06-17 17:37:22 -0400

    fixed threading

--- windsonic.py
+++ windsonic.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 from threading import Thread
 
-t0 = time.time() #Get innitial time 
+
 
 datTime = datetime.now() # current date and time
 dat = "hr_" + datTime.strftime("%H") + "_min_" + datTime.strftime("%M") + "_sec_" + datTime.strftime("%S")  + "d_" + datTime.strftime("%d") + "_m_" + datTime.strftime("%m") + "_y_" + datTime.strftime("%Y") + "_"
@@ -32,42 +32,56 @@ if log:
     f.write("DATA FORMAT\n")
     f.write("Format, Wind Direction, Wind Speed, Units, Status, Checksum\n")
 
-#DATA LOGGING SECTION
-ser = serial.Serial('/dev/ttyUSB0', 38400)
+
 
 def log():
-    if exitStat:
-        log_thread.join() #Kill thread
-    else:
-        try:
-            #Only log when switch is active
-            newDat = ser.readline()
-            newDat2 = newDat.replace(b"<STX>", b"")
-            newDat3 = newDat.replace(b"<ETX>", b"")
-            # byteChk = utf8len(newDat3)
-            toWrite = str(time.time() - t0) + "," + str(newDat3)
-            f.write(toWrite + "\n")
-            sys.stdout.write(toWrite+"\r")
-            sys.stdout.flush()
-        except KeyboardInterrupt:
-            print("Pressed Ctrl-C to terminate while statement")
+    while exit_thread.is_alive():
+        #print(exit_thread.is_alive())
+        if exitStat:
+            f.close()
+            time.sleep(1)
+            sys.exit()
+            break
+        else:
+            try:
+                #Only log when switch is active
+                newDat = ser.readline()
+                newDat2 = newDat.replace(b"<STX>", b"")
+                newDat3 = newDat.replace(b"<ETX>", b"")
+                # byteChk = utf8len(newDat3)
+                toWrite = str(time.time() - t0) + "," + str(newDat3)
+                f.write(toWrite + "\n")
+                sys.stdout.write(toWrite+"\r")
+                sys.stdout.flush()
+            except KeyboardInterrupt:
+                print("Pressed Ctrl-C to terminate while statement")
+
 
 def getExitStat():
     exitIn = input("Press 9 to terminate logging")
     if exitIn == "9":
         exitStat = True #Exit logging
-        exit_thread.join() #Kill thread
+        #exit_thread.join() #Kill thread
 
 
-
-log_thread = Thread(target=log)
 exit_thread = Thread(target=getExitStat)
+exit_thread.daemon = True
+log_thread = Thread(target=log)
+log_thread.daemon = True
+
 input("Serial connection established, press enter to begin logging")
-log_thread.start()
+t0 = time.time() #Get innitial time 
+#DATA LOGGING SECTION
+ser = serial.Serial('/dev/ttyUSB0', 38400)
 exit_thread.start()
+log_thread.start()
+
 
 while log_thread.is_alive() and exit_thread.is_alive():
     time.sleep(0.1)
+
+log_thread.join()
+exit_thread.join()
 
 #ENDGAME 
 f.close()                  #⛔⛔⛔⛔ CLOSE FILE
